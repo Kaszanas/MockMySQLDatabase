64a7087ef0d11e75a2220841045c287858d6b9eb
Kaszanas <34846245+Kaszanas@users.noreply.github.com>
2019-10-28 12:49:33 +0100

    Support for Environment Variables
    This change is important security-wise.
    It isn't optimal to hold password in plain text, that would be unnacceptable in production environment.

--- settings.py
+++ settings.py
@@ -1,19 +1,23 @@
 import platform
+import os
 
-CONNECTION_STRING = ''
-DATABASE_NAME = ''
+driver_string = 'mysql+pymysql'
+username = 'root'
 
-# Template:
-# CONNECTION_STRING = 'mysql+pymysql://root:<YourPassword>@localhost/'
+'''
+Template:
+CONNECTION_STRING = 'mysql+pymysql://username:password@localhost/'
+If there is a port required for connection it will look like this:
+CONNECTION_STRING = 'mysql+pymysql://username:password@localhost:port/'
 
-# BE ADVISED THAT STORING PASSWORDS IN PLAIN TEXT IS NOT THE BEST WAY!
-# THIS WAS DONE FOR THE SHOWCASING AND EASE OF USE PURPOSE.
-
-# Normally this would be done via Environement Variables.
+REMEMBER:
+Before using this script You will need to add Your password to the Environment Variables.
+You can either do it in Your system settings or using the console.
+'''
 
 if platform.node() == 'Your_PC_Name':
-    CONNECTION_STRING =
-    DATABASE_NAME =
-else:
+    CONNECTION_STRING = f'{driver_string}://{username}:{os.environ.get("DB_PASSWORD")@localhost/}'
+    DATABASE_NAME = 'Database'
+else: # If You wouldn't like to specify the PC name.
     CONNECTION_STRING =
     DATABASE_NAME =
